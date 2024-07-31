ebca343e9a904dbdc74e72acb3640b9d594be270
Rahul Rustagi <rrustagi20@iitk.ac.in>
2024-08-01 00:12:18 +0530

    resturcture description

--- description/start.sh
+++ description/start.sh
@@ -1,8 +1,19 @@
 export HUSKY_LMS1XX_ENABLED=1
 export HUSKY_REALSENSE_ENABLED=1
-# gnome-terminal -- rviz
-# gnome-terminal -- roslaunch description start.launch
+# source /home/rahul/ros/arloc/src/arloc/description/param_office.sh
+
+# For mapping
+
+# gnome-terminal -- roslaunch description office.launch
 # gnome-terminal -- roslaunch apriltag_ros continuous_detection.launch
-# gnome-terminal -- rqt_image_view /tag_detections_image
 # gnome-terminal -- roslaunch husky_navigation gmapping.launch
 # gnome-terminal -- rosrun teleop_twist_keyboard teleop_twist_keyboard.py
+# gnome-terminal -- rosbag record husky_pose
+# gnome-terminal -- rosrun map_server map_saver -f /home/rahul/ros/arloc/src/arloc/description/maps/office
+
+# For navigation
+
+# gnome-terminal -- rosbag play husky_pose.bag
+# gnome-terminal -- roslaunch husky_navigation amcl.launch
+# gnome-terminal -- rosrun map_server map_server /home/rahul/ros/arloc/src/arloc/description/maps/office.yaml
+# gnome-terminal -- rosbag record amcl_pose
